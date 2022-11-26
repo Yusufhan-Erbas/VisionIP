7ea67e25b8d7fa99214ecd7a0a7a2d2ce7e622f5
Yusufhan26 <114617848+Yusufhan26@users.noreply.github.com>
2022-11-27 00:05:27 +0300

    Add project files.

---
+++ VisionIP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VisionIP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
